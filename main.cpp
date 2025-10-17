26d999fcb4fd8b67371a028a9c7e52721239cdda
s1140327-afk <s1140327@dmhs.kh.edu.tw>
2025-10-17 16:19:05 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+#include <string>
+int main() {
+    int age =15;
+    double height = 170.5;
+    std::string name "6084"
+    bool is_pass = ture;
+    std::cout << " 姓名: " << NAME << "身高:" << height <<std::END1;
+    std::cout << " 年齡: " << age  << " 是否及格 "<<IN_PASS <<STD::END1;
+    return 0;
+}
