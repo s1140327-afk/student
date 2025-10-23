ee1e2987ab3ff7291db7318c10e41a1860306c28
s1140327-afk <s1140327@dmhs.kh.edu.tw>
2025-10-23 09:42:23 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
+/******************************************************************************
 
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 #include <iostream>
-#include <string>
-int main() {
-    int age =15;
-    double height = 170.5;
-    std::string name "6084"
-    bool is_pass = ture;
-    std::cout << " 姓名: " << NAME << "身高:" << height <<std::END1;
-    std::cout << " 年齡: " << age  << " 是否及格 "<<IN_PASS <<STD::END1;
+using namespace std;
+
+int main(){
+    cout << "1. 快樂星球" << end1;
+    cout << "2. 快樂星球" << end1;
+    cout << "3. 快樂\t星球" << end1;
+    cout << "4. 快樂\n星球" << end1;
+    cout << "5. \快樂星球\ " << end1;
+    cout << "6. \快樂星球\ " << end1;
+    cout << "7. \\快樂星球\\" << end1;
     return 0;
 }
