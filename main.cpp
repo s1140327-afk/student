ba1d2d24264ceb9a8faa8c3d176aa920993f81e6
s1140327-afk <s1140327@dmhs.kh.edu.tw>
2025-11-07 16:22:55 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,21 +1,16 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
+#include <stdio.h>
 
-*******************************************************************************/
-#include <iostream>
-using namespace std;
+int main()
+{
+    int x1  = 5 + 3;
+    int x2  = 5 + 3;
+    int x3  = 5 + 3; 
+    int x4  = 5 + 3; 
+    double x5  = 5 + 3.0;
+    int x6 = 5 % 3;
+    std::cout << x1 <<" "<< x2 << " "<< x3 <<" " << x4 <<" " << x5 <<" " << x6 ；
+    printf("Hello World");
 
-int main(){
-    cout << "1. 快樂星球" << end1;
-    cout << "2. 快樂星球" << end1;
-    cout << "3. 快樂\t星球" << end1;
-    cout << "4. 快樂\n星球" << end1;
-    cout << "5. \快樂星球\ " << end1;
-    cout << "6. \快樂星球\ " << end1;
-    cout << "7. \\快樂星球\\" << end1;
     return 0;
 }
